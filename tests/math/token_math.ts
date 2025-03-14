d001b6d094672bf3199f85c7bec1d173763b6c3d
burnanything <burnanything@proton.me>
2025-03-14 18:09:58 +0800

    fix lint

--- tests/math/token_math.ts
+++ tests/math/token_math.ts
@@ -18,28 +18,35 @@ export declare type CurveParams = {
 
 export const CURVE_1_PARAMS: CurveParams = {
   n: 4,
-  k_with_multiplier_sol: BigInt("70000000000000000000") * BigInt(web3.LAMPORTS_PER_SOL),
+  k_with_multiplier_sol:
+    BigInt("70000000000000000000") * BigInt(web3.LAMPORTS_PER_SOL),
   c_with_sol: BigInt("7000000000"),
   token_supply_at_boundary: (MAX_TOKEN_SUPPLY * BigInt(80)) / BigInt(100),
   native_amount_at_boundary: BigInt(10089843750),
 };
 export const CURVE_2_PARAMS: CurveParams = {
   n: 2,
-  k_with_multiplier_sol: BigInt("218750000000000000000") * BigInt(web3.LAMPORTS_PER_SOL),
+  k_with_multiplier_sol:
+    BigInt("218750000000000000000") * BigInt(web3.LAMPORTS_PER_SOL),
   c_with_sol: BigInt("24089843750"),
   token_supply_at_boundary: (MAX_TOKEN_SUPPLY * BigInt(5)) / BigInt(100),
   native_amount_at_boundary: BigInt(8725910156250),
 };
 export const CURVE_3_PARAMS: CurveParams = {
   n: 1,
-  k_with_multiplier_sol: BigInt("8750000000000000000000") * BigInt(web3.LAMPORTS_PER_SOL),
+  k_with_multiplier_sol:
+    BigInt("8750000000000000000000") * BigInt(web3.LAMPORTS_PER_SOL),
   c_with_sol: BigInt("8774089843750"),
   token_supply_at_boundary: BigInt(1),
   native_amount_at_boundary: BigInt("874999999999991225910156250"), // only 1 token left when calculating the curve's native_amount
 };
 export const CURVE_LAST_PARAMS = CURVE_3_PARAMS;
 
-export const CURVES: Array<CurveParams> = [CURVE_1_PARAMS, CURVE_2_PARAMS, CURVE_3_PARAMS];
+export const CURVES: Array<CurveParams> = [
+  CURVE_1_PARAMS,
+  CURVE_2_PARAMS,
+  CURVE_3_PARAMS,
+];
 
 export function find_root(
   remaining_token_supply: bigint,
@@ -50,7 +57,9 @@ export function find_root(
   let target_native_amount = remaining_token_supply_native_amount + pay_amount;
   if (params.k_with_multiplier_sol == CURVE_LAST_PARAMS.k_with_multiplier_sol) {
     let numerator = CURVE_LAST_PARAMS.k_with_multiplier_sol;
-    let denominator = (target_native_amount + CURVE_LAST_PARAMS.c_with_sol) * (MULTIPLIER / MAX_TOKEN_SUPPLY);
+    let denominator =
+      (target_native_amount + CURVE_LAST_PARAMS.c_with_sol) *
+      (MULTIPLIER / MAX_TOKEN_SUPPLY);
     let remaining_token_supply_target = ceil_div(numerator, denominator);
     if (remaining_token_supply_target >= remaining_token_supply) {
       throw new Error("Buy amount too large");
@@ -72,7 +81,11 @@ export function find_root(
   return remaining_token_supply - high;
 }
 
-export function calculate_curve(target_token_supply: bigint, round_up: boolean, params: CurveParams): bigint {
+export function calculate_curve(
+  target_token_supply: bigint,
+  round_up: boolean,
+  params: CurveParams
+): bigint {
   let pow_x: bigint;
   if (params.n > 1) {
     pow_x = pow(target_token_supply, BigInt(params.n), round_up);
@@ -93,19 +106,31 @@ export function search_curve(target_token_supply: bigint): CurveParams {
   return CURVE_LAST_PARAMS;
 }
 
-export function calculate_price(target_token_supply: bigint, y: bigint, curve: CurveParams): Decimal {
+export function calculate_price(
+  target_token_supply: bigint,
+  y: bigint,
+  curve: CurveParams
+): Decimal {
   let x_dec = new Decimal(target_token_supply.toString()).div(new Decimal(1e6));
-  let y_dec = new Decimal((y + curve.c_with_sol).toString()).div(new Decimal(web3.LAMPORTS_PER_SOL));
+  let y_dec = new Decimal((y + curve.c_with_sol).toString()).div(
+    new Decimal(web3.LAMPORTS_PER_SOL)
+  );
   let Decimal15 = Decimal.set({ precision: 15, rounding: Decimal.ROUND_DOWN });
   let n_dec = new Decimal15(curve.n);
   return n_dec.mul(y_dec).div(x_dec);
 }
 
 export function calculate_market_cap(price: Decimal): Decimal {
-  return price.mul(new Decimal(MAX_TOKEN_SUPPLY.toString()).div(new Decimal(1e6)));
+  return price.mul(
+    new Decimal(MAX_TOKEN_SUPPLY.toString()).div(new Decimal(1e6))
+  );
 }
 
-export function div_with_rounding(numerator: bigint, denominator: bigint, round_up: boolean): bigint {
+export function div_with_rounding(
+  numerator: bigint,
+  denominator: bigint,
+  round_up: boolean
+): bigint {
   let quotient = numerator / denominator;
 
   if (round_up && numerator % denominator != BigInt(0)) {
